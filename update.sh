f73b7a9d21a0755b755fb762477c01dd58b32a2c
Robert Loomans <robert@loomans.org>
2023-08-18 16:37:50 +1000

    update update.sh script again

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-set -e
+
+set -euo pipefail
 
 TAG="$(curl -sS 'https://api.github.com/repos/kovidgoyal/calibre/releases?per_page=1' | jq -r '.[0].tag_name')"
 
@@ -12,12 +13,16 @@ sed -i "s/ARG CALIBRE_RELEASE\=.*/ARG CALIBRE_RELEASE\=\"${TAG:1}\"/" Dockerfile
 
 if git diff --exit-code Dockerfile; then
     echo "Dockerfile already on ${TAG}"
-    exit 0
+
+    if [ "$(git tag --list "${TAG}")" != "${TAG}" ]; then
+      echo ${TAG} tag is missing
+      git tag "${TAG}"
+    fi
+else
+    git add Dockerfile
+    git commit -m "update Calibre to ${TAG}"
+    git tag --force "${TAG}"
 fi
 
-git add Dockerfile
-git commit -m "update Calibre to ${TAG}"
-git tag -f "${TAG}"
-
-git push --force-with-lease --tags
+git push --force --tags
 git push origin main
